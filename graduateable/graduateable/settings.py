758e5eefe2094b4e8176eda292cc7aad2c8d0394
kwmheen <kwmheen@gmail.com>
2024-01-24 17:08:32 +0900

    Debug False

--- graduateable/graduateable/settings.py
+++ graduateable/graduateable/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qk!ev1a7vo^yi@wk-hb#op6dsl#g2$(k)_o7!2@ox=$ebis(@p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
